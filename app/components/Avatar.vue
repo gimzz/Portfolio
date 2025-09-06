0463d5fd7d52a5c74879f5703e2c09330bfa0bf2
gimzz <letig732@gmail.com>
2025-09-05 23:25:43 -0400

    se crea el componente para manipular de mejor forma la imagen del avatar

---
+++ app/components/Avatar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import avatar from '~/assets/avatar.jpeg';
+</script>
+
+<template>
+  <img 
+    :src="avatar" 
+    alt="Avatar" 
+    class="w-36 h-36 rounded-full object-cover object-center border-[3px] border-violet-200 p-1"
+  />
+</template>
